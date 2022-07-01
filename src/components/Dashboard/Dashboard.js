0c32018a40d7015341c8086838b866294a8a6db5
samarth-gwalani <108356851+samarth-gwalani@users.noreply.github.com>
2022-07-01 12:40:21 -0400

    Initial Dashboard Setup

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Dashboard_Sidebar from '../Dashboard_Sidebar/Dashboard_Sidebar'
+import Sidebar from '../Sidebar/Sidebar'
 
 function Dashboard() {
   return (
@@ -14,6 +16,9 @@ function Dashboard() {
     <br/>
     <br/>
     <h1>Dashboard</h1>
+    
+    <Dashboard_Sidebar/>
+    <div className='homeContainer'>Container</div>
     </>
   )
 }
