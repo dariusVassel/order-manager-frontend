97910d3c0d28411fac0c26e963599fb802656e19
samarth-gwalani <108356851+samarth-gwalani@users.noreply.github.com>
2022-07-24 18:29:17 -0400

    Reducers Changes

--- src/actions/inquiries.js
+++ src/actions/inquiries.js
@@ -1,15 +1,61 @@
 import { baseUrl, headers, getToken } from '../Globals'
 
 
-export const addInquiry = (details ) => {
-    const payload=  {
-        ...details
-    }
-    return {
-        type: "ADD_INQUIRY",
-        payload
+export const addInquiry = (strongParams, navigate ) => {
+    //***no fetch***//
+    // const payload=  {
+    //     ...strongParams
+    // }
+    // return {
+    //     type: "ADD_INQUIRY",
+    //     payload
+    // }
+
+    //***for fetch***//
+    return async (dispatch) => {
+        dispatch({type : "REQUESTING"});
+
+        const resp = await fetch(baseUrl + '/order_inquiries',{
+            method: "POST",
+        headers: {
+            ...headers,
+            ...getToken()
+        },
+        body: JSON.stringify(strongParams)
+        })
+        const data = await resp.json();
+        console.log(data)
+        if(data.errors) {
+            dispatch({ type: "ERRORS", payload: data.errors })
+          } else {
+
+            dispatch({ type: "CLEAR_ERRORS" })
+            dispatch({type: "ADD_INQUIRY", payload: data})
+            navigate('/inquiries')
+          }
+          dispatch({ type: "DONE_REQUESTING" });
     }
 }
+
+export const deleteInquiry = (navigate, id ) => {
+  //***for fetch***//
+  return async (dispatch) => {
+      dispatch({type : "REQUESTING"});
+
+      const resp = await fetch(baseUrl + `/order_inquiries/${id}`, {
+        method: "DELETE",
+        headers: {
+          ...headers,
+          ...getToken()
+        }}).then((r) => {
+          if (r.ok) {
+            dispatch({type: "DELETE_INQUIRY", payload: id})
+          }
+        });
+        dispatch({ type: "DONE_REQUESTING" });
+  }
+}
+
 
 export const loadInquiries = () => {
     return async dispatch => {
@@ -22,6 +68,7 @@ export const loadInquiries = () => {
           })
 
           const data = await resp.json()
+    
           dispatch({type: "SET_INQUIRIES", payload: data})
           dispatch({type: "DONE_REQUESTING"})
     }
---
+++ src/actions/orders.js
@@ -0,0 +1,37 @@
+import { baseUrl, headers, getToken } from '../Globals'
+
+export const loadOrders = () => {
+    return async dispatch => {
+        dispatch({type: "REQUESTING"})
+        const resp = await fetch(baseUrl + '/orders', {
+            headers: {
+              ...headers,
+              ...getToken()
+            }
+          })
+
+          const data = await resp.json()
+    
+          dispatch({type: "SET_ORDERS", payload: data})
+          dispatch({type: "DONE_REQUESTING"})
+    }
+}
+
+export const deleteOrder = (navigate, id ) => {
+  //***for fetch***//
+  return async (dispatch) => {
+      dispatch({type : "REQUESTING"});
+
+      const resp = await fetch(baseUrl + `/orders/${id}`, {
+        method: "DELETE",
+        headers: {
+          ...headers,
+          ...getToken()
+        }}).then((r) => {
+          if (r.ok) {
+            dispatch({type: "DELETE_ORDER", payload: id})
+          }
+        });
+        dispatch({ type: "DONE_REQUESTING" });
+  }
+}
